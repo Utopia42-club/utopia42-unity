492eba963a77e382c03e78e8ec5be0a0b3cb0017
Rasoul Mirzaiezadeh <mirzaierasoul75@gmail.com>
2021-06-05 12:03:03 +0430

    Add Ethereum client

---
+++ Assets/src/Service/Ethereum/EthereumClientService.cs
@@ -0,0 +1,16 @@
+﻿using Nethereum.JsonRpc.UnityClient;
+using Smart_contracts.Contracts.Utopia.ContractDefinition;
+using System.Collections.Generic;
+
+public class EthereumClientService
+{
+    private static string NETWORK_URL = "https://mainnet.infura.io/v3/b12c1b1e6b2e4f58af559a67fe46104e";
+    private static string CONTRACT_ADDRESS = "0x56040d44f407fa6f33056d4f352d2e919a0d99fb";
+
+    public List<string> getOwners()
+    {
+        var queryRequest = new QueryUnityRequest<GetOwnersFunction, GetOwnersOutputDTOBase>(NETWORK_URL, CONTRACT_ADDRESS);
+        queryRequest.Query(new GetOwnersFunction() { }, CONTRACT_ADDRESS);
+        return queryRequest.Result.ReturnValue1;
+    }
+}
