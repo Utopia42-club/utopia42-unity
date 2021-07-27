b095ec414d63762fd3fd39474f6ed8863be4fde5
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2021-07-27 10:09:19 +0430

    Add action buttons and joystick

---
+++ Assets/src/Canvas/ActionButton.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ActionButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool pressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+
+}
