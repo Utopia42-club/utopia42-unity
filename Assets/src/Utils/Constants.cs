226f51c220ecc55365887bd2f385d541bd6b6d01
Rasoul Mirzaiezadeh <mirzaierasoul75@gmail.com>
2022-01-06 16:58:11 +0330

    Update version

--- Assets/src/Utils/Constants.cs
+++ Assets/src/Utils/Constants.cs
@@ -3,7 +3,9 @@ namespace src.Utils
     public static class Constants
     {
         public static readonly string ApiURL = "https://utopiapi.vitaminhq.ir";
-        public static readonly string NetsURL = ApiURL + "/static/networks.json";
         public static readonly string WebAppHomeURL = "https://utopia.vitaminhq.ir/home";
+        // public static readonly string ApiURL = "https://api.utopia42.club";
+        // public static readonly string WebAppHomeURL = "https://app.utopia42.club/home";
+        public static readonly string NetsURL = ApiURL + "/static/networks.json";
     }
 }
