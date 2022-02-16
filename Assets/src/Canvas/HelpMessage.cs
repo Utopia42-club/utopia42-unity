a620b420dec0936b5684804000d43da66a7a2e99
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-16 15:50:52 +0330

    Fix HelpMessage visibility problem

--- Assets/src/Canvas/HelpMessage.cs
+++ Assets/src/Canvas/HelpMessage.cs
@@ -9,23 +9,27 @@ namespace src.Canvas
 
         void Start()
         {
-            GameManager.INSTANCE.stateChange.AddListener(s =>
+            var gameManager = GameManager.INSTANCE;
+            gameManager.stateChange.AddListener(UpdateView);
+            UpdateView(gameManager.GetState());
+        }
+
+        private void UpdateView(GameManager.State s)
+        {
+            switch (s)
             {
-                switch (s)
-                {
-                    case (GameManager.State.PLAYING):
-                        gameObject.SetActive(true);
-                        textMesh.SetText("F2: Open Settings");
-                        break;
-                    case (GameManager.State.MAP):
-                        gameObject.SetActive(true);
-                        textMesh.SetText("F2: Open Side Panel");
-                        break;
-                    default:
-                        gameObject.SetActive(false);
-                        break;
-                }
-            });
+                case (GameManager.State.PLAYING):
+                    gameObject.SetActive(true);
+                    textMesh.SetText("F2: Open Settings");
+                    break;
+                case (GameManager.State.MAP):
+                    gameObject.SetActive(true);
+                    textMesh.SetText("F2: Open Side Panel");
+                    break;
+                default:
+                    gameObject.SetActive(false);
+                    break;
+            }
         }
     }
 }
