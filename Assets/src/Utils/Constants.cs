d831fdfd09f26c3315eafa4374b3fb3a37e3cde0
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-03-07 22:24:44 +0330

    Update version and urls

--- Assets/src/Utils/Constants.cs
+++ Assets/src/Utils/Constants.cs
@@ -2,12 +2,12 @@ namespace src.Utils
 {
     public static class Constants
     {
-        // public static readonly string WebAppBaseURL = "https://utopia.vitaminhq.ir";
-        public static readonly string WebAppBaseURL = "https://app.utopia42.club";
+        public static readonly string WebAppBaseURL = "https://utopia.vitaminhq.ir";
+        // public static readonly string WebAppBaseURL = "https://app.utopia42.club";
         // public static readonly string WebAppBaseURL = "http://localhost:4200";
 
-        // public static readonly string ApiURL = "https://utopiapi.vitaminhq.ir";
-        public static readonly string ApiURL = "https://api.utopia42.club";
+        public static readonly string ApiURL = "https://utopiapi.vitaminhq.ir";
+        // public static readonly string ApiURL = "https://api.utopia42.club";
 
         public static readonly string NetsURL = ApiURL + "/static/networks.json";
         public static readonly string WebAppHomeURL = WebAppBaseURL + "/home";
