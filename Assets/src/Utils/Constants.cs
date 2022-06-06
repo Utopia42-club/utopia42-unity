f837d3e1955c43e2aab5e9fb586a3ff2402395ea
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-06-06 09:18:46 +0430

    Revert Constants file

--- Assets/src/Utils/Constants.cs
+++ Assets/src/Utils/Constants.cs
@@ -6,11 +6,11 @@ namespace src.Utils
         // public static readonly string WebAppBaseURL = "https://app.utopia42.club";
         // public static readonly string WebAppBaseURL = "http://localhost:4200";
 
-        // public static readonly string ApiURL = "https://utopiapi.vitaminhq.ir";
+        public static readonly string ApiURL = "https://utopiapi.vitaminhq.ir";
         // public static readonly string ApiURL = "https://api.utopia42.club";
-        public static readonly string ApiURL = "http://localhost:8080";
+        // public static readonly string ApiURL = "http://localhost:8080";
 
-        public static readonly string NetsURL = "https://utopiapi.vitaminhq.ir" + "/static/networks.json";
+        public static readonly string NetsURL = ApiURL + "/static/networks.json";
         // public static readonly string NetsURL = "https://api.utopia42.club" + "/static/networks.json";
         public static readonly string WebAppHomeURL = WebAppBaseURL + "/home";
     }
