23bc161cea62cda0f90554bef6126a24cfdbf36e
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-06-29 12:08:33 +0430

    Show integer coordinates in the map view

--- Assets/Source/Ui/Map/MapActionsLayer.cs
+++ Assets/Source/Ui/Map/MapActionsLayer.cs
@@ -1,5 +1,6 @@
 using Source.Ui.Popup;
 using Source.Ui.Utils;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Source.Ui.Map
@@ -13,18 +14,23 @@ namespace Source.Ui.Map
             showYourLocationButton.clickable.clicked += map.MoveToPlayerPosition;
             showYourLocationButton.tooltip = "Show your location";
             showYourLocationButton.AddManipulator(new ToolTipManipulator(Side.TopLeft));
+
             var zoomInButton = this.Q<Button>("zoomInButton");
             zoomInButton.AddManipulator(new ToolTipManipulator(Side.TopLeft));
             zoomInButton.clickable.clicked += map.ZoomIn;
+
             var zoomOutButton = this.Q<Button>("zoomOutButton");
             zoomOutButton.AddManipulator(new ToolTipManipulator(Side.TopLeft));
             zoomOutButton.clickable.clicked += map.ZoomOut;
+
             var currentLocationLabel = this.Q<Label>("currentLocationLabel");
-            currentLocationLabel.text = Player.INSTANCE.GetPosition().ToString();
+            var pos = Player.INSTANCE.GetPosition();
+            currentLocationLabel.text = new Vector3Int((int) pos.x, (int) pos.y, (int) pos.z).ToString();
             var currentLocationBox = this.Q<VisualElement>("currentLocationBox");
             currentLocationBox.tooltip = "Click to copy";
             currentLocationBox.AddManipulator(new ToolTipManipulator(Side.TopLeft));
             currentLocationBox.RegisterCallback<MouseMoveEvent>(evt => GameManager.INSTANCE.CopyPositionLink());
+
             style.position = Position.Absolute;
             style.overflow = Overflow.Visible;
             style.width = style.height = 0;
