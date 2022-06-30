0069cf254f2e1975fb049ab6e452f0a961c6059e
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-06-30 07:39:48 +0430

    Refactor login page ui

--- Assets/Source/Ui/Login/Login.cs
+++ Assets/Source/Ui/Login/Login.cs
@@ -1,8 +1,6 @@
-using System.Linq;
 using Source.Service.Ethereum;
 using Source.Ui.Dialog;
 using Source.Ui.Snack;
-using Source.Ui.Utils;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Position = Source.Model.Position;
@@ -17,12 +15,16 @@ namespace Source.Ui.Login
 
         private Vector3? startingPosition = null;
         private VisualElement root;
-        private VisualElement walletLoginTile;
+        private VisualElement memberTile;
+        private VisualElement guestTile;
+        private Button guestTabButton;
+        private Button memberTabButton;
 
         private void OnEnable()
         {
             root = GetComponent<UIDocument>().rootVisualElement;
-            walletLoginTile = root.Q<VisualElement>("walletLoginTile");
+            memberTile = root.Q<VisualElement>("memberTile");
+            guestTile = root.Q<VisualElement>("guestTile");
 
             guestButton = root.Q<Button>("guestButton");
             guestButton.clickable.clicked += () =>
@@ -34,11 +36,13 @@ namespace Source.Ui.Login
             submitButton = root.Q<Button>("enterButton");
             submitButton.clickable.clicked += Submit;
 
+            memberTabButton = root.Q<Button>("memberTabButton");
+            memberTabButton.clickable.clicked += () => SelectTab(0);
+            guestTabButton = root.Q<Button>("guestTabButton");
+            guestTabButton.clickable.clicked += () => SelectTab(1);
+
             var loading = LoadingLayer.LoadingLayer.Show(root);
-            GameManager.INSTANCE.StartCoroutine(EthNetwork.GetNetworks(_ =>
-                {
-                    loading.Close();
-                },
+            GameManager.INSTANCE.StartCoroutine(EthNetwork.GetNetworks(_ => { loading.Close(); },
                 () =>
                 {
                     SnackService.INSTANCE.Show(
@@ -49,6 +53,26 @@ namespace Source.Ui.Login
                     );
                     loading.Close();
                 }));
+            SelectTab(0);
+        }
+
+        private void SelectTab(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    guestTile.style.display = DisplayStyle.None;
+                    memberTile.style.display = DisplayStyle.Flex;
+                    guestTabButton.RemoveFromClassList("selected-login-tab-button");
+                    memberTabButton.AddToClassList("selected-login-tab-button");
+                    break;
+                case 1:
+                    guestTile.style.display = DisplayStyle.Flex;
+                    memberTile.style.display = DisplayStyle.None;
+                    guestTabButton.AddToClassList("selected-login-tab-button");
+                    memberTabButton.RemoveFromClassList("selected-login-tab-button");
+                    break;
+            }
         }
 
         private void Submit()
@@ -57,7 +81,7 @@ namespace Source.Ui.Login
                 OpenCredentialsDialog();
             else
             {
-                var metamaskLoading = LoadingLayer.LoadingLayer.Show(walletLoginTile);
+                var metamaskLoading = LoadingLayer.LoadingLayer.Show(memberTile);
                 AuthService.Connect(detail =>
                 {
                     metamaskLoading.Close();
