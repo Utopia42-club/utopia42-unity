b547beead41797511da6971fb24ab163bc8e4c90
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-07-11 11:48:16 +0430

    Change urls and version for main server deploy

--- Assets/Source/Utils/Constants.cs
+++ Assets/Source/Utils/Constants.cs
@@ -2,14 +2,14 @@ namespace Source.Utils
 {
     public static class Constants
     {
-        public static readonly string WebAppBaseURL = "https://dev.utopia42.club";
+        // public static readonly string WebAppBaseURL = "https://dev.utopia42.club";
         // public static readonly string WebAppBaseURL = "http://utopia.vitaminhq.ir";
-        // public static readonly string WebAppBaseURL = "https://app.utopia42.club";
+        public static readonly string WebAppBaseURL = "https://app.utopia42.club";
         // public static readonly string WebAppBaseURL = "http://localhost:4200";
 
-        public static readonly string ApiURL = "https://demoapi.utopia42.club";
+        // public static readonly string ApiURL = "https://demoapi.utopia42.club";
         // public static readonly string ApiURL = "https://utopiapi.vitaminhq.ir";
-        // public static readonly string ApiURL = "https://api.utopia42.club";
+        public static readonly string ApiURL = "https://api.utopia42.club";
         // public static readonly string ApiURL = "http://localhost:8080";
 
         public static readonly string NetsURL = ApiURL + "/static/networks.json";
